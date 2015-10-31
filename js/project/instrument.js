81de6d80bd91199d266c21b0b943fc99adda90dc
sarmeet singh <mail.sarmeet@gmail.com>
2015-10-31 19:23:01 -0400

    capturing heights and width of the settings and setting canvas accordingly.

--- js/project/instrument.js
+++ js/project/instrument.js
@@ -2,12 +2,14 @@
  * Created by sarmeetsingh on 10/30/15.
  */
 
+var canvasId;
 
-
-function grab_element_create_canvas(name){
-    $(name).append("hwlloooo")
-
+function grab_element_create_canvas(name, container, height_container){
+    canvasId = $(name);
+    canvasId.css("border","dotted 1px");
+    canvasId.css("width", $(container).width());
+    canvasId.css("height", $(height_container).height());
+    console.log($(container).width())
 }
 
-
-grab_element_create_canvas("#instrument");
+grab_element_create_canvas("#instrument_canvas", "#instrument_row", "#settings_row");
