48dae15a900964ab6f2cd04849d2aefe18a611c1
sarmeet singh <mail.sarmeet@gmail.com>
2015-10-31 23:54:01 -0400

    added context

--- js/project/instrument.js
+++ js/project/instrument.js
@@ -1,15 +1,22 @@
 /**
  * Created by sarmeetsingh on 10/30/15.
  */
+{
+    var canvasId;
 
-var canvasId;
+    function grab_element_create_canvas(name, container, height_container) {
+        canvasId = $(name);
+        canvasId.css("border", "dotted 1px");
+        canvasId.css("width", $(container).width());
+        canvasId.css("height", $(height_container).height());
 
-function grab_element_create_canvas(name, container, height_container){
-    canvasId = $(name);
-    canvasId.css("border","dotted 1px");
-    canvasId.css("width", $(container).width());
-    canvasId.css("height", $(height_container).height());
-    console.log($(container).width())
+        var canvas2DContext = canvasId[0].getContext("2d");
+        canvas2DContext.beginPath();
+        canvas2DContext.arc(100,75,50,0,4*Math.PI)
+        canvas2DContext.stroke();
+
+
+    }
+
 }
-
 grab_element_create_canvas("#instrument_canvas", "#instrument_row", "#settings_row");
